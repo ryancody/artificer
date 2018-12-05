2933c22ccd07bf8116ac866b889710d60fb226a0
Ryan Cody <ryan.p.cody@gmail.com>
2018-12-05 15:13:06 -0500

    updated styling for bulma

--- src/components/Web3Info.jsx
+++ src/components/Web3Info.jsx
@@ -5,10 +5,10 @@ let Web3Info = (props) => {
     
   return (
     <div className='web3info container' onClick={props.onClick}>
-      <h2>Web3 Info</h2>
-      <h3>Account: {account.slice(0,6) + '...' + account.slice(account.length-6, account.length)}</h3>
-      <h3>ETH Balance: {props.balance}</h3>
-      <h3>Block: {props.block}</h3>
+      <div className='title is-5'>Web3 Info</div>
+      <div>Account: {account.slice(0,6) + '...' + account.slice(account.length-6, account.length)}</div>
+      <div>ETH Balance: {props.balance}</div>
+      <div>Block: {props.block}</div>
     </div>
   )
 }
