66f6a0ca47b3ac38e5c245c5ee803607dca45ce2
ryancody <ryan.p.cody@gmail.com>
2019-10-21 23:41:27 -0400

    formatted metamask warning, added link to metamask

--- src/components/Web3Info.jsx
+++ src/components/Web3Info.jsx
@@ -2,31 +2,27 @@ import React from 'react'
 import {account, web3, isLoggedIn} from './Web3Instance'
 
 let Web3Info = (props) => {
-  let web3warning
+  let display
   
   if(!isLoggedIn){
-    web3warning = <div className='notification is-warning'>
-      <strong>Need Web3!</strong>  Please log in to MetaMask
+    display = <div className='notification is-warning'>
+      <strong>Need Web3!</strong>  Please log in to <a target='_blank' href='https://metamask.io/'>MetaMask</a>
     </div>
+  } else {
+    display = <div>
+      <div className='title is-5'>Web3 Info</div>
+      <div className='subtitle'>      
+        <p><strong>Account</strong> {account.slice(0,6) + '...' + account.slice(account.length-6, account.length)}</p>
+        <p><strong>ETH</strong> {props.balance}</p>
+        <p><strong>Block</strong> {props.block}</p>
+      </div>
+    </div> 
   }
 
   return (
 
-    <div className='web3info container' onClick={props.onClick}>
-      <div className='columns'>
-        <div className='column is-half'>
-          <div className='title is-5'>Web3 Info</div>
-          <div className='subtitle'>      
-            <p><strong>Account</strong> {account.slice(0,6) + '...' + account.slice(account.length-6, account.length)}</p>
-            <p><strong>ETH</strong> {props.balance}</p>
-            <p><strong>Block</strong> {props.block}</p>
-          </div>
-        </div>
-        <div className='column is-one-third'>
-          {web3warning}
-        </div>
-      </div>
-
+    <div className='web3info column' onClick={props.onClick}>
+      {display}
     </div>
   )
 }
